89e59f99f2b9c3eedb6a2c263a91216bee4c6b74
bokang03 <bogang12345@gmail.com>
2026-02-05 12:14:34 +0900

    test : Rq 리팩토링(블루 단계)

--- src/main/java/Rq.java
+++ src/main/java/Rq.java
@@ -6,11 +6,6 @@ public class Rq {
     }
 
     public String getActionName(){
-        if(cmd.equals("삭제?id=1")){
-            return "삭제";
-        } else if(cmd.equals("수정?id=1")){
-            return "수정";
-        }
-        return "";
+        return cmd.split("\\?")[0];
     }
 }
