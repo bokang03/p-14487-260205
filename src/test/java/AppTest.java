1b39321e7a1818e562c2101fbb2a43dd782b6500
bokang03 <bogang12345@gmail.com>
2026-02-05 11:17:35 +0900

    test : 실패 케이스 작성

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,15 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    void t1(){
+        App app = new App();
+        int rst = app.plus(1, 2);
+
+        // 테스트 코드 값이 3이여야 한다. 맞으면 Pass, 틀리면 Fail(다시 작성해야함)
+        Assertions.assertThat(rst).isEqualTo(3);
+    }
+}
+
