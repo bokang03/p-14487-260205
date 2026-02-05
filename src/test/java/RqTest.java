43359781be26d38b8656aeb3b55257a59b8d9030
bokang03 <bogang12345@gmail.com>
2026-02-05 13:38:06 +0900

    test : 명령을 수행할 key의 value 찾는 기능 테스트 코드 작성(레드 단계)

--- src/test/java/RqTest.java
+++ src/test/java/RqTest.java
@@ -7,20 +7,40 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class RqTest {
 
     @Test
-    @DisplayName(value = "rq.getActionName()")
-    void t1(){
+    @DisplayName(value = "명령: 삭제?id=1 일때, rq.getActionName() - 삭제")
+    void t1() {
 
         Rq rq = new Rq("삭제?id=1");
         String actionName = rq.getActionName();
         assertThat(actionName).isEqualTo("삭제");
+
     }
 
     @Test
-    @DisplayName(value = "rq.getActionName()")
-    void t2(){
+    @DisplayName(value = "명령: 수정?id=1 일때, rq.getActionName() - 수정")
+    void t2() {
 
         Rq rq = new Rq("수정?id=1");
         String actionName = rq.getActionName();
         assertThat(actionName).isEqualTo("수정");
+
+    }
+
+    @Test
+    @DisplayName(value = "명령: 목록?keyword=자바 일때, rq.getParam(\"kewyord\") -> \"자바\"")
+    void t3() {
+
+        Rq rq = new Rq("목록?keyword=자바");
+        String rst = rq.getParam("keyword");
+        assertThat(rst).isEqualTo("자바");
+    }
+
+    @Test
+    @DisplayName(value = "명령: 목록?keywordType=title 일때, rq.getParam(\"kewyordType\") -> \"title\"")
+    void t4() {
+
+        Rq rq = new Rq("목록?keywordType=title");
+        String rst = rq.getParam("keywordType");
+        assertThat(rst).isEqualTo("title");
     }
 }
